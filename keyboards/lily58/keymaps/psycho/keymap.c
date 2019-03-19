27eff8ebf9600f794a2476d0e748c72558f78cfb
inoues <psychobmx.com+github@gmail.com>
2019-03-19 20:59:42 +0900

    日本語キーボート[]対応

--- keyboards/lily58/keymaps/psycho/keymap.c
+++ keyboards/lily58/keymaps/psycho/keymap.c
@@ -23,11 +23,13 @@ extern uint8_t is_master;
 #define _RAISE 2
 #define _ADJUST 16
 
-//KC_QUOT : and +
+//KC_QUOT : : and +
 //KC_LBRC : @ and `
 //KC_EQL  : ^ and ~
 //KC_RO   : \ and _ (EN mark and UNder score)
 //KC_JYEN : \ and | (EN mark and Vertical Line)
+//KC_RBRC : [ and {
+//KC_BSLS : ] and }
 #define JP_DQT  LSFT(KC_2) // "
 #define KC_ALPS LALT(KC_PSCR) // Alt + PrintScreen
 
@@ -60,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_RO), \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_RBRC ,  KC_BSLS ,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_RO), \
                              KC_LALT, KC_LGUI,RAISE, KC_SPC,   KC_ENT,   LOWER,   KC_BSPC, KC_APP \
 ),
 /* LOWER
