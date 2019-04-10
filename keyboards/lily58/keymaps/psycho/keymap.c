a726dfe6a87b65f1b2d56f7efcec75e202781ada
inoues <psychobmx.com+github@gmail.com>
2019-04-10 22:01:08 +0900

    appをDelに変更、変換、無変換を割り当て

--- keyboards/lily58/keymaps/psycho/keymap.c
+++ keyboards/lily58/keymaps/psycho/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+-------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |  ,<  |  .>  |  /?  |RShift |
  * `-----------------------------------------/       /     \      \------------------------------------------'
- *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP|  APP |
+ *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP|  DEL |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      JP_AT, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, JP_LBRC,  JP_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-                             KC_LGUI, KC_LALT,LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_APP \
+                             KC_LGUI, KC_LALT,LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_DEL \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,------------------------------------------.
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+-------|
  * |LShift|      |      |M_LBTN|M_RBTN|      |-------|    |-------|   /  |   0  |   0  |      |      |   _   |
  * `-----------------------------------------/       /     \      \------------------------------------------'
- *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP|  APP |
+ *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP|  DEL |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -95,21 +95,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,------------------------------------------.
  * | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+-------|
- * | Tab  |      |      |  WU  |      |      |                    |      |  Ps  | AlPs |      |C+A+D | F12   |
+ * | Tab  |      |      |  WU  |      |      |                    |      |  Ps  | AlPs | INS  |C+A+D | F12   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+-------|
- * |LCTRL |      |  WL  |  WD  |  WR  |      |-------.    ,-------|      |  INS | Home |      |  UP  |       |
+ * |LCTRL |      |  WL  |  WD  |  WR  |      |-------.    ,-------|      | PgUp | Home |      |  UP  |       |
  * |------+------+------+------+------+------| CPgUp |    | CPgDn |------+------+------+------+------+-------|
- * |LShift|      |      |      |      |      |-------|    |-------| AL+UP|  DEL |  END | LEFT | DOWN | RGHT  |
+ * |LShift|      |      |      |      |      |-------|    |-------| AL+UP| PgDn |  END | LEFT | DOWN | RGHT  |
  * `-----------------------------------------/       /     \      \------------------------------------------'
- *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP|  APP |
+ *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP|  DEL |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_RAISE] = LAYOUT( \
   KC_ESC , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11, \
-  _______, XXXXXXX, XXXXXXX, KC_WINU, XXXXXXX, XXXXXXX,                    XXXXXXX, KC_PSCR, KC_ALPS, XXXXXXX, KC_CAD,   KC_F12, \
-  _______, XXXXXXX, KC_WINL, KC_WIND, KC_WINR, XXXXXXX,                    XXXXXXX,  KC_INS, KC_HOME, XXXXXXX,  KC_UP,  XXXXXXX, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CPUP, KC_CPDN,  KC_ALUP,  KC_DEL,  KC_END, KC_LEFT, KC_DOWN, KC_RGHT, \
+  _______, XXXXXXX, XXXXXXX, KC_WINU, XXXXXXX, XXXXXXX,                    XXXXXXX, KC_PSCR, KC_ALPS,  KC_INS, KC_CAD,   KC_F12, \
+  _______, XXXXXXX, KC_WINL, KC_WIND, KC_WINR, XXXXXXX,                    XXXXXXX, KC_PGUP, KC_HOME, XXXXXXX,  KC_UP,  XXXXXXX, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CPUP, KC_CPDN,  KC_ALUP, KC_PGDN,  KC_END, KC_LEFT, KC_DOWN, KC_RGHT, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 /* ADJUST
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP|  APP |
+ *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP|  DEL |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -204,6 +204,9 @@ void iota_gfx_task_user(void) {
 }
 #endif//SSD1306OLED
 
+static bool lower_pressed = false;
+static bool raise_pressed = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
 #ifdef SSD1306OLED
@@ -221,21 +224,41 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case LOWER:
       if (record->event.pressed) {
+        lower_pressed = true; // (2)
+
         layer_on(_LOWER);
         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+
+        if (lower_pressed) { // (4)
+          register_code(KC_LANG2); // for macOS
+          register_code(KC_MHEN);
+          unregister_code(KC_MHEN);
+          unregister_code(KC_LANG2);
+        }
+        lower_pressed = false;
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
+        raise_pressed = true;
+
         layer_on(_RAISE);
         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+
+        if (raise_pressed) { // (4)
+          register_code(KC_LANG1); // for macOS
+          register_code(KC_HENK);
+          unregister_code(KC_HENK);
+          unregister_code(KC_LANG1);
+        }
+        raise_pressed = false;
       }
       return false;
       break;
