fd1ae0301b128c358c4ac5a40fc023aece9417f8
Shinsuke Inoue <psychobmx.com@gmail.com>
2019-12-10 00:16:36 +0900

    #2 chage Raise/Lower lang1,2

--- keyboards/crkbd/keymaps/psycho/keymap.c
+++ keyboards/crkbd/keymaps/psycho/keymap.c
@@ -45,16 +45,6 @@ enum macro_keycodes {
 #define KC_CPUP LCTL(KC_PGUP)      // ctl + pgup
 #define KC_ALUP LALT(KC_UP)        // alt + up
 
-// JP_MHEN KC_INT5 // muhenkan
-// JP_HENK KC_INT4 // henkan
-// JP_KANA KC_INT2 // katakana/hiragana|ro-mazi
-
-// JP_MKANA KC_LANG1 //kana on MacOSX
-// JP_MEISU KC_LANG2 //eisu on MacOSX
-
-#define KC_LOWR LT(LOWER, JP_MHEN)    // タップで無変換     ホールドでLower
-#define KC_RASE LT(RAISE, JP_HENK)    // タップで変換       ホールドでRaise
-
 #define KC_TBSF  LSFT_T(KC_TAB)
 // #define KC_SPSF  LSFT_T(KC_SPC)
 #define KC_ALAP  LALT_T(KC_APP)
@@ -208,6 +198,11 @@ void iota_gfx_task_user(void) {
 }
 #endif//SSD1306OLED
 
+static bool lower_pressed = false;
+static uint16_t lower_pressed_time = 0;
+static bool raise_pressed = false;
+static uint16_t raise_pressed_time = 0;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
 #ifdef SSD1306OLED
@@ -224,20 +219,42 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
     case LOWER:
       if (record->event.pressed) {
+        lower_pressed = true;
+        lower_pressed_time = record->event.time;
+
         layer_on(_LOWER);
         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+
+        if (lower_pressed && (TIMER_DIFF_16(record->event.time, lower_pressed_time) < TAPPING_TERM)) {
+          register_code(KC_LANG2); // for macOS
+          register_code(KC_MHEN);
+          unregister_code(KC_MHEN);
+          unregister_code(KC_LANG2);
+        }
+        lower_pressed = false;
       }
       return false;
     case RAISE:
       if (record->event.pressed) {
+        raise_pressed = true;
+        raise_pressed_time = record->event.time;
+
         layer_on(_RAISE);
         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+
+        if (raise_pressed && (TIMER_DIFF_16(record->event.time, raise_pressed_time) < TAPPING_TERM)) {
+          register_code(KC_LANG1); // for macOS
+          register_code(KC_HENK);
+          unregister_code(KC_HENK);
+          unregister_code(KC_LANG1);
+        }
+        raise_pressed = false;
       }
       return false;
     case ADJUST:
